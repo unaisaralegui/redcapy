9aec061d06a34aee7f0d8582f263a6d3788b0245
Unai Saralegui <usaralegui@gmail.com>
2019-11-01 19:26:08 +0100

    Create keywords.py

---
+++ redcapy/keywords.py
@@ -0,0 +1,28 @@
+__author__ = "Unai Saralegui"
+__copyright__ = "Copyright 2019, Unai Saralegui"
+__credits__ = ["Unai Saralegui", "Josu Gomez"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Unai Saralegui"
+__email__ = "usaralegui@gmail.com"
+__status__ = "Development"
+
+
+# FORMAT keywords
+FORMAT_JSON = "json"
+
+# CONTENT keywords
+CONTENT_ARM = "arm"
+CONTENT_EVENT = "event"
+CONTENT_FIELD_NAMES = "exportFieldNames"
+CONTENT_FILE = "file"
+CONTENT_FORM_EVENT_MAPPING = "formEventMapping"
+CONTENT_INSTRUMENT = "instrument"
+CONTENT_METADATA = "metadata"
+CONTENT_PROJECT_XML = "project_xml"
+CONTENT_RECORD = "record"
+
+# FIELDS KEYWORDS
+TOKEN = "token"
+CONTENT = "content"
+FORMAT = "returnFormat"
