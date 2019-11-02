3c311407ae4c8b5fa84ddbbaaeb4d30afeca1cee
Unai Saralegui <usaralegui@gmail.com>
2019-11-02 16:20:59 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="redcapy",
+    version="0.1",
+    author="Unai Saralgui",
+    author_email="usaralegui@gmail.com",
+    description="Python package to access REDCAP API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/unaisaralegui/redcapy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
