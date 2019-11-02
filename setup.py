5a6a5909ac236344b94be899748c841d1bcc43d5
Unai Saralegui <usaralegui@gmail.com>
2019-11-02 16:33:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://gitlab.com/unaisaralegui/redcapy",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'lxml',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
