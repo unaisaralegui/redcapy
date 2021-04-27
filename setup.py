029f4b51c17e6d248e72e382253afd76d1eb3f10
Saralegui Vallejo, Unai <unai.saralegui@tecnalia.com>
2021-04-27 08:53:12 +0200

    redo project variable declaration

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 import setuptools
+from distutils.util import convert_path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+metadata = {}
+ver_path = convert_path('redcapy/metadata.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), metadata)
+
+
 setuptools.setup(
     name="redcapy",
-    version="0.4",
-    author="Unai Saralegui",
-    author_email="usaralegui@gmail.com",
+    version=metadata['__version__'],
+    author=metadata['__author__'],
+    author_email=metadata['__email__'],
     description="Python package to access REDCAP API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.com/unaisaralegui/redcapy",
+    url="https://github.com/unaisaralegui/redcapy",
     packages=setuptools.find_packages(),
     install_requires=[
         'requests',
